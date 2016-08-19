07859f2b0bf0692c8ee53c8d280473f3e2a1ff69
manojbalaji87 <ushabalaji.erd@gmail.com>
2016-08-19 10:14:04 +0530

    Create String_to_int.java

---
+++ String_to_int.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class String_to_int
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sb=new Scanner(System.in);
+  String a=new String();
+  a=sb.next();
+  int n=Integer.parseInt(p);
+  System.out.println("String to integer conversion:"+n);
+		
+	}
+}
